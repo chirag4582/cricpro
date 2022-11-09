c28440258d0507fdc2abac1615f073c664298a29
chirag4582 <115335660+chirag4582@users.noreply.github.com>
2022-11-09 16:40:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+const one = document.getElementById('1')
+const two = document.getElementById('2')
+const three = document.getElementById('3')
+const four = document.getElementById('4')
+const five = document.getElementById('5')
+const six = document.getElementById('6')
+const seven = document.getElementById('7')
+const eight = document.getElementById('8')
+const nine = document.getElementById('9')
+
+var sehwag = document.getElementById('sehwag')
+var sachin = document.getElementById('sachin')
+var gautam = document.getElementById('gambhir')
+var kohli = document.getElementById('virat')
+var dhoni = document.getElementById('dhoni')
+var yuvraj = document.getElementById('yuvraj')
+var raina = document.getElementById('raina')
+var bhaji = document.getElementById('bhaji')
+var zaheer = document.getElementById('zaheer')
+// const show = document.getElementsByClassName('show')
+// console.log(sehwag)
+var closeButt = document.getElementById('close')
+var closeButt2 = document.getElementById('close2')
+var closeButt3 = document.getElementById('close3')
+var closeButt4 = document.getElementById('close4')
+var closeButt5 = document.getElementById('close5')
+var closeButt6= document.getElementById('close6')
+var closeButt7 = document.getElementById('close7')
+var closeButt8 = document.getElementById('close8')
+var closeButt9 = document.getElementById('close9')
+
+// console.log(one)
+one.onclick = () =>
+{
+    sehwag.style.display="block"
+}
+closeButt.onclick = () => 
+{
+    sehwag.style.display="none"
+}
+
+two.onclick = () =>
+{
+    sachin.style.display="block"
+}
+closeButt2.onclick = () => 
+{
+    sachin.style.display="none"
+}
+three.onclick = () =>
+{
+    gautam.style.display="block"
+}
+closeButt3.onclick = () => 
+{
+    gautam.style.display="none"
+}
+four.onclick = () =>
+{
+    kohli.style.display="block"
+}
+closeButt4.onclick = () => 
+{
+    kohli.style.display="none"
+}
+five.onclick = () =>
+{
+    dhoni.style.display="block"
+}
+closeButt5.onclick = () => 
+{
+    dhoni.style.display="none"
+}
+six.onclick = () =>
+{
+    yuvraj.style.display="block"
+}
+closeButt6.onclick = () => 
+{
+    yuvraj.style.display="none"
+}
+seven.onclick = () =>
+{
+    raina.style.display="block"
+}
+closeButt7.onclick = () => 
+{
+    raina.style.display="none"
+}
+eight.onclick = () =>
+{
+    bhaji.style.display="block"
+}
+closeButt8.onclick = () => 
+{
+    bhaji.style.display="none"
+}
+nine.onclick = () =>
+{
+    zaheer.style.display="block"
+}
+closeButt9.onclick = () => 
+{
+    zaheer.style.display="none"
+}
+
+
+
